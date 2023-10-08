3e90fe1141ab51972ab5ce2246560f66fde97d91
Diab16 <abdohdiab166@gmail.com>
2023-10-09 00:18:08 +0300

    implementing the routers/tasks.js

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,4 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const {
+  getAllTasks,
+  creatTask,
+  getTask,
+  updateTask,
+  deleteTask,
+} = require("../controllers");
+router.route("/").get(getAllTasks).post(creatTask);
+router.route("/:id").get(getTask).patch(updateTask).delete(deleteTask);
 
 module.exports = router;
