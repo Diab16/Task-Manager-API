aac8e899826371141cba26d7d87ebbcfd3eb5237
Diab16 <abdohdiab166@gmail.com>
2023-10-08 22:21:49 +0300

    the initial structure of the project folders

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+require("dotenv").config();
+
+// init app
+const app = express();
+
+// apply middlewares
+app.use(express.json);
+
+// Routers
+app.use("/api/v1/tasks", require("./routes/tasks"));
+
+// Running the Server
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(
+    `Hello, Server is running in ${process.env.NODE} mode on port ${PORT}`
+  );
+});
