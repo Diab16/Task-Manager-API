2c1cd550223a35f73a61fa5bf550c8d79608bbf8
Diab16 <abdohdiab166@gmail.com>
2023-10-13 02:45:24 +0300

    fix bug in error message and status code

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,4 +1,5 @@
 const errorHandler = (err, req, res, next) => {
-  return res.status(500).json({ message: err });
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode).json({ message: err.message });
 };
 module.exports = errorHandler;
